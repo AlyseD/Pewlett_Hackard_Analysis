5c072f159e316eb71d9d2d0f5786157b0f80dc4c
AlyseD <67987035+AlyseD@users.noreply.github.com>
2020-09-02 21:53:52 -0700

    updated 7.3.1 schema queries

--- Queries/schema_activity.sql
+++ Queries/schema_activity.sql
@@ -40,8 +40,8 @@ CREATE TABLE titles (
   title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+  PRIMARY KEY (emp_no, title, from_date)
 );
 
 CREATE TABLE dept_employees (
@@ -53,3 +53,11 @@ CREATE TABLE dept_employees (
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
   PRIMARY KEY (emp_no, dept_no)
 )
+
+);
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
