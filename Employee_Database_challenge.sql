c41069fd9cbad7381fec84e359d1b3f78acfe245
AlyseD <67987035+AlyseD@users.noreply.github.com>
2020-09-06 12:30:20 -0700

    Module 7 Challenge Queries

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -24,3 +24,24 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(unique_titles.title) DESC
+
+
+
+SELECT DISTINCT ON (emp_no) employees.emp_no,
+employees.first_name,
+employees.last_name,
+employees.birth_date,
+dept_employees.from_date,
+dept_employees.to_date,
+titles.title
+INTO mentorship_eligibility
+FROM employees
+    INNER JOIN dept_employees
+        ON (employees.emp_no = dept_employees.emp_no)
+    INNER JOIN titles
+        ON (employees.emp_no = titles.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no ASC;
+
+SELECT * from mentorship_eligibility
+
